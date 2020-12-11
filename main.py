44521d1392a8b73f27f3119356b7a103875220bb
MorganWenjunYang <ecbm4040@instance-1.c.ecbm4040-wy2347.internal>
2020-12-11 23:57:12 +0000

    add some comment and helpful code

--- main.py
+++ main.py
@@ -5,9 +5,51 @@ Created on Fri Dec  4 22:28:33 2020
 @author: YWJ97
 """
 
-def train:
+'''
+def train():
     pass
 
+######################### Code that might be useful
+######################### Copy from DCGAN turtorial
+# https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/generative/dcgan.ipynb#scrollTo=CA1w-7s2POEy
+
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+
+def train(dataset, epochs):
+  for epoch in range(epochs):
+    start = time.time()
+
+    for image_batch in dataset:
+      train_step(image_batch)
+
+    # Produce images for the GIF as we go
+    display.clear_output(wait=True)
+    generate_and_save_images(generator,
+                             epoch + 1,
+                             seed)
+########################################################################
+    # Save the model every 15 epochs
+    if (epoch + 1) % 15 == 0:
+      checkpoint.save(file_prefix = checkpoint_prefix)
+########################################################################
+    print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+
+  # Generate after the final epoch
+  display.clear_output(wait=True)
+  generate_and_save_images(generator,
+                           epochs,
+                           seed)
+
+
+'''
 
 if __name__ == "__main__":
     pass
